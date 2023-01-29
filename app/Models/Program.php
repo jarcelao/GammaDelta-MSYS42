a0355ffcff2b4453c366d42d608b18cf8d84bdbc
nik1082 <jerichoarcelao@gmail.com>
2023-01-29 21:39:24 +0800

    Create Program model and migration

---
+++ app/Models/Program.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'group_id',
+        'title',
+        'purpose',
+        'indicators',
+        'start_date',
+        'end_date',
+        'assumptions_and_risks',
+        'inputs',
+        'activities',
+        'outputs',
+        'outcomes',
+        'why',
+        'status',
+    ];
+
+    /**
+     * Get the user that owns the program.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the group assigned to the program.
+     */
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+}
