a1bf38f6939a0681c3eec71f7350744cdf3cb41c
nik1082 <jerichoarcelao@gmail.com>
2023-01-29 21:14:33 +0800

    Add team model and migration

---
+++ app/Models/Team.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'people_group_id',
+        'team_leader',
+    ];
+
+    /**
+     * The attributes for which you can use filters in url.
+     *
+     * @var array
+     */
+    protected $allowedFilters = [
+        'id',
+        'people_group_id',
+        'team_leader',
+    ];
+
+    /**
+     * The attributes for which can use sort in url.
+     *
+     * @var array
+     */
+    protected $allowedSorts = [
+        'id',
+        'people_group_id',
+        'team_leader',
+    ];
+}
